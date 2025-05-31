3a68d5e9cbcc433856e31330817051d636ae7f2a
Ivan <social@hubbu.net>
2025-05-30 23:15:43 -0400

    Adjust key width to fit enter button

--- app/src/main/java/net/hubbu/kotlin_worlde/ui/composable/Keyboard.kt
+++ app/src/main/java/net/hubbu/kotlin_worlde/ui/composable/Keyboard.kt
@@ -8,11 +8,16 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -20,6 +25,7 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import net.hubbu.kotlin_worlde.R
 
 enum class KeyboardIcon(val res: Int, val description: String) {
@@ -31,16 +37,29 @@ fun KeyboardButton(modifier: Modifier = Modifier, text: String? = null, icon: Ke
     Box(
         contentAlignment = Alignment.Center,
         modifier = modifier
-            .defaultMinSize(minWidth = 28.dp, minHeight = 56.dp)
+            .defaultMinSize(minWidth = 30.dp, minHeight = 56.dp)
+            .widthIn(min = 0.dp, max = 48.dp)
             .clip(RoundedCornerShape(4.dp))
             .background(color = MaterialTheme.colorScheme.primaryContainer)
-            .padding(horizontal = 8.dp)
+            .padding(horizontal = 4.dp)
             .wrapContentSize(),
     ) {
+        val style = MaterialTheme.typography.titleMedium
+        var fontSize by remember {
+            mutableStateOf(style.fontSize)
+        }
+
         if (text != null) {
             Text(
                 text = "$text",
-                style = MaterialTheme.typography.titleMedium,
+                style = style,
+                maxLines = 1,
+                fontSize = fontSize,
+                onTextLayout = {
+                    if (it.multiParagraph.didExceedMaxLines) {
+                        fontSize *= .8F
+                    }
+                }
             )
         }
         if (icon != null) {
