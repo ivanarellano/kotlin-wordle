636da5711f2ed9f5d6060641bbde573beae76e0c
Ivan <social@hubbu.net>
2025-05-30 22:57:35 -0400

    Add enter and backspace keys

--- app/src/main/java/net/hubbu/kotlin_worlde/ui/composable/Keyboard.kt
+++ app/src/main/java/net/hubbu/kotlin_worlde/ui/composable/Keyboard.kt
@@ -1,11 +1,13 @@
 package net.hubbu.kotlin_worlde.ui.composable
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.defaultMinSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
@@ -14,33 +16,56 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import net.hubbu.kotlin_worlde.R
+
+enum class KeyboardIcon(val res: Int, val description: String) {
+    Delete(R.drawable.outline_backspace_24, "Backspace")
+}
 
 @Composable
-fun KeyboardLetter(character: Char, modifier: Modifier = Modifier) {
+fun KeyboardButton(modifier: Modifier = Modifier, text: String? = null, icon: KeyboardIcon? = null) {
     Box(
         contentAlignment = Alignment.Center,
         modifier = modifier
             .defaultMinSize(minWidth = 28.dp, minHeight = 56.dp)
             .clip(RoundedCornerShape(4.dp))
             .background(color = MaterialTheme.colorScheme.primaryContainer)
+            .padding(horizontal = 8.dp)
             .wrapContentSize(),
     ) {
-        Text(
-            text = "$character",
-            style = MaterialTheme.typography.titleMedium,
-        )
+        if (text != null) {
+            Text(
+                text = "$text",
+                style = MaterialTheme.typography.titleMedium,
+            )
+        }
+        if (icon != null) {
+            Image(
+                painter = painterResource(id = icon.res),
+                contentDescription = icon.description,
+                contentScale = ContentScale.Fit
+            )
+        }
     }
 }
 
 @Composable
 fun Keyboard(modifier: Modifier = Modifier) {
     @Composable
-    fun KeyRow(keys: String) {
+    fun KeyRow(keys: String, isBottomRow: Boolean = false) {
         Row(horizontalArrangement = Arrangement.spacedBy(6.dp)) {
+            if (isBottomRow) {
+                KeyboardButton(text = "ENTER")
+            }
             for (char in keys) {
-                KeyboardLetter(character = char)
+                KeyboardButton(text = "$char")
+            }
+            if (isBottomRow) {
+                KeyboardButton(icon = KeyboardIcon.Delete)
             }
         }
     }
@@ -52,7 +77,7 @@ fun Keyboard(modifier: Modifier = Modifier) {
     ) {
         KeyRow("QWERTYUIOP")
         KeyRow("ASDFGHJKL")
-        KeyRow("ZXCVBNM")
+        KeyRow(keys ="ZXCVBNM", isBottomRow = true)
     }
 }
 
