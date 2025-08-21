74d8f3dadcd69913ac5240a061058ae47058d82f
Ivan <social@hubbu.net>
2025-08-20 20:08:07 -0400

    Extract LetterModel class

---
+++ app/src/main/java/net/hubbu/kotlin_wordle/ui/LetterModel.kt
@@ -0,0 +1,44 @@
+package net.hubbu.kotlin_wordle.ui
+
+import net.hubbu.kotlin_wordle.ui.composable.LetterType
+import net.hubbu.kotlin_wordle.ui.theme.GameColor
+
+sealed class LetterModel(
+    open val char: Char,
+    open val bgColor: GameColor,
+    open val borderColor: GameColor,
+) {
+    abstract val type: LetterType
+
+    class Empty(
+        override val char: Char = ' ',
+        override val bgColor: GameColor = GameColor.Transparent,
+        override val borderColor: GameColor = GameColor.MaterialPrimary
+    ) : LetterModel(char, bgColor, borderColor) {
+        override val type: LetterType = LetterType.Empty
+    }
+
+    class Correct(
+        override val char: Char,
+        override val bgColor: GameColor = GameColor.Green,
+        override val borderColor: GameColor = GameColor.Transparent
+    ) : LetterModel(char, bgColor, borderColor) {
+        override val type: LetterType = LetterType.Correct
+    }
+
+    class Incorrect(
+        override val char: Char,
+        override val bgColor: GameColor = GameColor.Yellow,
+        override val borderColor: GameColor = GameColor.Transparent
+    ) : LetterModel(char, bgColor, borderColor) {
+        override val type: LetterType = LetterType.Absent
+    }
+
+    class Partial(
+        override val char: Char,
+        override val bgColor: GameColor = GameColor.MaterialPrimary,
+        override val borderColor: GameColor = GameColor.Transparent
+    ) : LetterModel(char, bgColor, borderColor) {
+        override val type: LetterType = LetterType.Present
+    }
+}
