e237b4eb4f05e24fdcf25fced0770259f4c78daf
Ivan <social@hubbu.net>
2025-09-16 22:39:50 -0400

    Add animated end game message to GameScreen

--- app/src/main/java/net/hubbu/kotlin_wordle/ui/composable/GameScreen.kt
+++ app/src/main/java/net/hubbu/kotlin_wordle/ui/composable/GameScreen.kt
@@ -1,11 +1,17 @@
 package net.hubbu.kotlin_wordle.ui.composable
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Info
 import androidx.compose.material.icons.filled.Settings
@@ -19,9 +25,12 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
 import net.hubbu.kotlin_wordle.data.LetterModel
@@ -35,6 +44,7 @@ fun GameScreen(modifier: Modifier = Modifier) {
     GameScreen(
         guessedWords = uiState.guessedWords,
         currentWord = uiState.currentWord,
+        isGameOver = uiState.isGameOver,
         targetWordMatches = viewModel.targetWordMatches,
         maxWordLength = viewModel.maxWordLength,
         maxWordCount = viewModel.maxWordCount,
@@ -51,6 +61,7 @@ fun GameScreen(modifier: Modifier = Modifier) {
 fun GameScreen(
     guessedWords: List<String>,
     currentWord: String,
+    isGameOver: Boolean,
     targetWordMatches: Map<Char, List<Int>>,
     maxWordLength: Int,
     maxWordCount: Int,
@@ -88,29 +99,75 @@ fun GameScreen(
             )
         },
     ) { innerPadding ->
-        Column(
-            modifier = Modifier.padding(innerPadding)
+        Box(
+            modifier = Modifier
+                .padding(innerPadding)
         ) {
-            Board(
-                guessedWords = guessedWords,
-                currentWord = currentWord,
-                targetWordMatches = targetWordMatches,
-                maxWordLength = maxWordLength,
-                maxWordCount = maxWordCount,
+            AnimatedVisibility(
+                visible = isGameOver,
+                enter = fadeIn(),
+                exit = fadeOut(),
                 modifier = Modifier
-                    .fillMaxWidth()
-                    .weight(2.5f),
-            )
-            Keyboard(
-                keyMatches = keyMatches,
-                onKeyPress = onKeyPress,
-                onEnter = onEnter,
-                onDelete = onDelete,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .weight(1f)
-            )
+                    .align(Alignment.TopCenter) // Position it at the top center
+                    .padding(top = 8.dp) // Adjust padding to move it down
+            ) {
+                EndGameMessage(guessedWords.size)
+            }
+            
+            Column {
+                Board(
+                    guessedWords = guessedWords,
+                    currentWord = currentWord,
+                    targetWordMatches = targetWordMatches,
+                    maxWordLength = maxWordLength,
+                    maxWordCount = maxWordCount,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .weight(2.5f),
+                )
+                Keyboard(
+                    keyMatches = keyMatches,
+                    onKeyPress = onKeyPress,
+                    onEnter = onEnter,
+                    onDelete = onDelete,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .weight(1f)
+                )
+            }
         }
+    }
+}
+@Composable
+fun EndGameMessage(guessCount: Int, modifier: Modifier = Modifier) {
+    // 1: Genius 2: Magnificent 3: Impressive 4: Splendid 5: Great 6: Phew
+    val text = when (guessCount) {
+        1 -> "Genius"
+        2 -> "Magnificent"
+        3 -> "Impressive"
+        4 -> "Splendid"
+        5 -> "Great"
+        6 -> "Phew"
+        else -> ""
+    }
+
+    return Box(
+        contentAlignment = Alignment.Center,
+        modifier = modifier
+            .clip(RoundedCornerShape(4.dp))
+            .background(Color.DarkGray)
+            .width(86.dp)
+            .height(56.dp)
+    ) {
+        Text(text, color = Color.White)
+    }
+}
+
+@Preview(showBackground = false)
+@Composable
+fun EndGameMessagePreview() {
+    KotlinWordleTheme {
+        EndGameMessage(guessCount = 1)
     }
 }
 
@@ -126,6 +183,7 @@ fun AppPreview() {
                 "PLANT",
             ),
             currentWord = "AUD",
+            isGameOver = true,
             targetWordMatches = mapOf(
                 'A' to listOf(0),
                 'U' to listOf(1),
